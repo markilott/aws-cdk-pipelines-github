5e13fec5c4d62482c2d4bcde7c01538521a89293
manuelqg <manuelqg@amazon.com>
2022-09-13 15:13:08 -0500

    test

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,8 @@
 import { Options } from '../types';
 
 export const options: Options = {
-    repoString: 'mygithub/', // GitHub owner/repository
-    connectionArn: 'arn:aws:codestar-connections:ap-southeast-1:444444444444:connection/xxxxxxxxxxxxxxxxxx', // CodeStar GitHub connection ARN
+    repoString: 'manuelqg/aws-cdk-pipelines', // GitHub owner/repository
+    connectionArn: 'arn:aws:codestar-connections:us-east-1:900126642232:connection/171ba7d9-5a14-4e6a-bfba-b1ddcccc1ca0', // CodeStar GitHub connection ARN
     version: '1', // Increment to trigger a pipeline deployment
     defaultRegion: 'us-east-1',
     toolsAccount: '900126642232', // Account where Pipelines are deployed
@@ -10,16 +10,21 @@ export const options: Options = {
         {
             name: 'dev', // Dev environment
             account: '984906628668',
-            region: 'us-west-1'
             branch: 'dev',
-            preApproval: false, // Require approval before Create Change Set
+            preApproval: false // Require approval before Create Change Set
         },
         {
             name: 'prod', // Production environment
             account: '581994098444',
-            region: 'us-west-2',
             branch: 'main',
-            preApproval: true,
+            preApproval: true
         },
-    ],
+        {
+            name: 'prod', // Production environment
+            account: '581994098444',
+            region: 'us-west-1',
+            branch: 'main',
+            preApproval: true
+        }
+    ]
 };
