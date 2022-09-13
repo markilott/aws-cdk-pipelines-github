726afef99f19648bceff6458d970ea15b707fa57
manuelqg <manuelqg@amazon.com>
2022-09-13 15:28:24 -0500

    Add event to payload

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ import { Options } from '../types';
 export const options: Options = {
     repoString: 'manuelqg/aws-cdk-pipelines', // GitHub owner/repository
     connectionArn: 'arn:aws:codestar-connections:us-east-1:900126642232:connection/171ba7d9-5a14-4e6a-bfba-b1ddcccc1ca0', // CodeStar GitHub connection ARN
-    version: '1', // Increment to trigger a pipeline deployment
+    version: '2', // Increment to trigger a pipeline deployment
     defaultRegion: 'us-east-1',
     toolsAccount: '900126642232', // Account where Pipelines are deployed
     appPipelines: [
