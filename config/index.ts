4434e1aa43c5e143d1abc279363106248ab7d1ea
manuelqg <manuelqg@amazon.com>
2022-09-13 13:40:25 -0500

    configure accounts

--- config/index.ts
+++ config/index.ts
@@ -1,34 +1,23 @@
 import { Options } from '../types';
 
 export const options: Options = {
-    repoString: 'mygithub/myrepo', // GitHub owner/repository
+    repoString: 'mygithub/', // GitHub owner/repository
     connectionArn: 'arn:aws:codestar-connections:ap-southeast-1:444444444444:connection/xxxxxxxxxxxxxxxxxx', // CodeStar GitHub connection ARN
     version: '1', // Increment to trigger a pipeline deployment
-    defaultRegion: 'ap-southeast-1',
-    toolsAccount: '444444444444', // Account where Pipelines are deployed
+    defaultRegion: 'us-east-1',
+    toolsAccount: '900126642232', // Account where Pipelines are deployed
     appPipelines: [
         {
             name: 'dev', // Dev environment
-            account: '111111111111',
+            account: '984906628668',
+            region: 'us-west-1'
             branch: 'dev',
             preApproval: false, // Require approval before Create Change Set
         },
         {
-            name: 'uat', // UAT/QA environment
-            account: '222222222222',
-            branch: 'uat',
-            preApproval: false,
-        },
-        {
             name: 'prod', // Production environment
-            account: '333333333333',
-            branch: 'main',
-            preApproval: true,
-        },
-        {
-            name: 'prod', // Production environment
-            account: '333333333333',
-            region: 'ap-southeast-2',
+            account: '581994098444',
+            region: 'us-west-2',
             branch: 'main',
             preApproval: true,
         },
